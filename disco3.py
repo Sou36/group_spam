cd38625e0c16b84f9c10ba205c7d25f49980c017
sou36 <135430197+Sou36@users.noreply.github.com>
2025-08-24 10:03:55 +0900

    Update disco3.py

--- disco3.py
+++ disco3.py
@@ -40,7 +40,7 @@ with sync_playwright() as p:
      #抜ける確認ボタン
      page.wait_for_selector(f"xpath=//button/div[normalize-space(text())='グループから脱退する']",timeout=10000)
      page.click(f"xpath=//button/div[normalize-space(text())='グループから脱退する']")
-     if count % 10 == 0:##10回おきに待った回数×1分+6分待機
+     if count > 0 and count % 10 == 0:##10回おきに待った回数×1分+6分待機
        time = datetime.now().strftime("%H:%M:%S")
        print(f"待機開始{time}。現在の待機時間は{count2 + 6}分です。")
        page.wait_for_timeout(count2 * 60000 + 360000)
@@ -50,6 +50,7 @@ with sync_playwright() as p:
      count += 1
     print(f"{kaisuu}回グループにターゲットを入れて抜けました。終了します。")
     browser.close()
+
 
 
 
